fae4cd90e23827e4f8c43b31b1bd7f3335063e61
jch007 <2350970240@qq.com>
2020-09-03 11:07:12 +0800

    Update RotToaxis.m

--- RotToaxis.m
+++ RotToaxis.m
@@ -3,7 +3,6 @@ function [theta,K]=RotToAxis(R)
 % [theta,k] = RotToAxis(R) converts a 3D rotation matrix, R, into the corresponding
 % axis anle. R is an 3-by-3-by-N matrix containing N rotation
 % matrices. The output, theta, is a rotation angles.K is a 3-by-1 rotation axis.
-% Rotation angles are in radians.
   r11=R(1,1);r12=R(1,2);r13=R(1,3);
   r21=R(2,1);r22=R(2,2);r23=R(2,3);
   r31=R(3,1);r32=R(3,2);r33=R(3,3);
