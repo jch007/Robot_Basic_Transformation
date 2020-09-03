381f0a89ca26a023af7595ef8df85e3f2802f157
jch007 <2350970240@qq.com>
2020-09-03 13:03:57 +0800

    Update RotToEul.m

--- RotToEul.m
+++ RotToEul.m
@@ -1,4 +1,6 @@
 function [alpha,beta,gamma] = RotToEul(rotm)
+% RotToEul convert rotation matrix to Euler angles,The Euler angles are specified by the axis rotation sequence,The default sequence of this function is ‘ZYX’
+% [alpha,beta,gamma]  = RotToEul(rotm) converts a 3D rotation matrix, rotm, into the corresponding Euler angle.
 r11=rotm(1,1);r12=rotm(1,2);r13=rotm(1,3);
 r21=rotm(2,1);r22=rotm(2,2);r23=rotm(2,3);
 r31=rotm(3,1);r32=rotm(3,2);r33=rotm(3,3);
